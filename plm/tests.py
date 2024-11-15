e9c609f4009336f4052ac3fb01afc4767b0228d3
thomas <deponte.thomas@gmail.com>
2024-11-16 00:50:32 +0100

    refactor testing

--- plm/tests.py
+++ plm/tests.py
@@ -3,37 +3,34 @@ from .models import Category, PartCategory, DocumentCategory, Part, Manufacturer
 
 # Create your tests here.
 class PartModelTest(TestCase):
-    def test_part_creation(self):
-        # Create a new part and verify its attributes
-        category = PartCategory.objects.create(name='MEC')
-        purchase_option = PurchaseOption.objects.create()
-        part = Part.objects.create(description='Test part', purchase_option=purchase_option, category=category)
-        self.assertEqual(part.internal_part_number, 'HW-MEC-001-v01r01')
-        self.assertFalse(part.released)
-        
+    def setUp(self):
+        self.category = PartCategory.objects.create(name='TST')
+        self.purchase_option = PurchaseOption.objects.create()
+        self.part = Part.objects.create(description='Test part', purchase_option=self.purchase_option, category=self.category)
+        self.assertEqual(self.part.internal_part_number, 'HW-TST-001-v01r01')
     def test_new_item_number(self):
-        # Create a new part and verify its attributes
-        category = PartCategory.objects.create(name='MEC')
-        purchase_option = PurchaseOption.objects.create()
-        first_part = Part.objects.create(description='Test part', purchase_option=purchase_option, category=category)
-        second_part = Part.objects.create(description='Test part', purchase_option=purchase_option, category=category)
-        self.assertEqual(second_part.internal_part_number, 'HW-MEC-002-v01r01')
-        self.assertFalse(second_part.released)
-        
+        next_part = Part.objects.create(description='Test part', purchase_option=self.purchase_option, category=self.category)
+        self.assertEqual(next_part.internal_part_number, 'HW-TST-002-v01r01')
     def test_new_variant(self):
-        # Create a new part and verify its attributes
-        category = PartCategory.objects.create(name='MEC')
-        purchase_option = PurchaseOption.objects.create()
-        first_part = Part.objects.create(description='Test part', purchase_option=purchase_option, category=category)
-        second_part = Part.objects.create(description='Test part', purchase_option=purchase_option, category=category, item_number=1)
-        self.assertEqual(second_part.internal_part_number, 'HW-MEC-001-v02r01')
-        self.assertFalse(second_part.released)
-        
+        next_part = Part.objects.create(description='Test part', purchase_option=self.purchase_option, category=self.category, item_number=1)
+        self.assertEqual(next_part.internal_part_number, 'HW-TST-001-v02r01')
     def test_new_revision(self):
-        # Create a new part and verify its attributes
-        category = PartCategory.objects.create(name='MEC')
-        purchase_option = PurchaseOption.objects.create()
-        first_part = Part.objects.create(description='Test part', purchase_option=purchase_option, category=category)
-        second_part = Part.objects.create(description='Test part', purchase_option=purchase_option, category=category, item_number=1, variant=1)
-        self.assertEqual(second_part.internal_part_number, 'HW-MEC-001-v01r02')
-        self.assertFalse(second_part.released)
+        next_part = Part.objects.create(description='Test part', purchase_option=self.purchase_option, category=self.category, item_number=1, variant=1)
+        self.assertEqual(next_part.internal_part_number, 'HW-TST-001-v01r02')
+    def test_edit_part_ipn_attributes(self):
+        new_category = PartCategory.objects.create(name='CAT')
+        with self.assertRaises(ValueError):
+            self.part.category = new_category
+            self.part.save()
+
+        with self.assertRaises(ValueError):
+            self.part.item_number = 123
+            self.part.save()
+
+        with self.assertRaises(ValueError):
+            self.part.variant = 456
+            self.part.save()
+
+        with self.assertRaises(ValueError):
+            self.part.revision = 789
+            self.part.save()
