1a95aa842e1243415aea1a41bf66e4f86a425cb7
thomas <deponte.thomas@gmail.com>
2024-11-16 00:26:10 +0100

    improved logic for parts ipn generation

--- plm/models.py
+++ plm/models.py
@@ -32,41 +32,72 @@ class Part(models.Model):
     variant = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     revision = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     image = models.BinaryField(null=True, blank=True)
+
     def __str__(self):
         return self.internal_part_number
-    
+
     def save(self, *args, **kwargs):
-        if not self.item_number:
-            last_part = Part.objects.filter(category=self.category).order_by('-item_number').first()
-            if last_part:
-                self.item_number = last_part.item_number + 1
-            else:
-                self.item_number = 1
-        if not self.variant:
-            last_part = Part.objects.filter(category=self.category, item_number=self.item_number).order_by('-variant').first()
-            if last_part:
-                self.variant = last_part.variant + 1
-            else:
-                self.variant = 1
-        if not self.revision:
-            last_part = Part.objects.filter(category=self.category, item_number=self.item_number, variant=self.variant).order_by('-revision').first()
-            if last_part:
-                self.revision = last_part.revision + 1
-            else:
-                self.revision = 1
         if not self._state.adding:  # Check if the instance is being created or updated
             original_part = Part.objects.get(pk=self.pk)
-            if original_part.internal_part_number != self.internal_part_number:
-                raise ValueError("Direct setting of internal_part_number is not allowed.")
-        # Calculate the internal_part_number based on other fields
+            if (original_part.category != self.category or
+                    original_part.item_number != self.item_number or
+                    original_part.variant != self.variant or
+                    original_part.revision != self.revision):
+                raise ValueError("Direct editing of category, item_number, variant, and revision is not allowed for existing parts.")
+        else:
+            if not Category:
+                raise ValueError("Part Category undefined")
+            if not self.item_number:
+                self.create_new_item()
+            if not self.variant:
+                self.create_new_variant()
+            if not self.revision:
+                self.create_new_revision()
+            self._update_internal_part_number()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if self.item_number < 1 or self.variant < 1 or self.revision < 1:
+            raise ValidationError("Fields item_number, variant, and revision must be greater than or equal to 1.")
+
+    def create_new_item(self, category=None):
+        if category is not None:
+            self.category = category
+        last_part = Part.objects.filter(category=self.category).order_by('-item_number').first()
+        if last_part:
+            self.item_number = last_part.item_number + 1
+        else:
+            self.item_number = 1
+        self.variant = 1
+        self.revision = 1
+    def create_new_variant(self, category=None, item_number=None):
+        if category is not None:
+            self.category = category
+        if item_number is not None:
+            self.item_number = item_number
+        last_part = Part.objects.filter(category=self.category, item_number=self.item_number).order_by('-variant').first()
+        if last_part:
+            self.variant = last_part.variant + 1
+        else:
+            self.variant = 1
+        self.revision = 1
+    def create_new_revision(self, category=None, item_number=None, variant=None):
+        if category is not None:
+            self.category = category
+        if item_number is not None:
+            self.item_number = item_number
+        if variant is not None:
+            self.variant = variant
+        last_part = Part.objects.filter(category=self.category, item_number=self.item_number, variant=self.variant).order_by('-revision').first()
+        if last_part:
+            self.revision = last_part.revision + 1
+        else:
+            self.revision = 1
+    def _update_internal_part_number(self):
         item_number_str = str(self.item_number).zfill(3)
         variant_str = str(int(self.variant)).zfill(2)
         revision_str = str(int(self.revision)).zfill(2)
         self.internal_part_number = f'HW-{self.category}-{item_number_str}-v{variant_str}r{revision_str}'
-        super().save(*args, **kwargs)
-    def clean(self):
-        if self.item_number < 1 or self.variant < 1 or self.revision < 1:
-            raise ValidationError("Fields item_number, variant, and revision must be greater than or equal to 1.")
 
 class Manufacturer(models.Model):
     id = models.AutoField(primary_key=True)
